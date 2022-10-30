ecdcb587860e197e7eb1c5985c84d52fa6d12106
shhady <shhadyse@gmail.com>
2022-10-30 13:28:44 +0200

    normal

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000"],
+    origin: "*",
   },
 });
 import path from "path";
