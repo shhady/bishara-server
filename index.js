df3f650209f1c4622b76b754c1c93fea3271222b
shhady <shhadyse@gmail.com>
2022-10-30 14:04:28 +0200

    test1

--- index.js
+++ index.js
@@ -17,10 +17,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "https://eclectic-kitten-7d4c01.netlify.app/",
+    origin: "/",
     methods: ["GET", "POST"],
-    allowedHeaders: ["my-custom-header"],
-    credentials: true,
   },
 });
 import path from "path";
