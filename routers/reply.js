b2ae55306829f82f08ab5a2322d5692246e06e4e
shhady <shhadyse@gmail.com>
2022-12-06 01:50:17 +0200

    back

--- routers/reply.js
+++ routers/reply.js
@@ -1,39 +1,39 @@
-import express from "express";
-const router = express.Router();
-import Reply from "../models/Reply.js";
-router.post("/replies", (req, res) => {
-  //   const course = new Course(req.body);
-  const reply = new Reply({
-    ...req.body,
-  });
-  try {
-    reply.save();
-    res.status(201).send(reply);
-  } catch (error) {
-    res.status(400).send(error);
-  }
-});
-router.get("/replies", async (req, res) => {
-  try {
-    const replies = await Reply.find({}).sort({ createdAt: +1 });
-    res.status(200).send(replies);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
+// import express from "express";
+// const router = express.Router();
+// import Reply from "../models/Reply.js";
+// router.post("/replies", (req, res) => {
+//   //   const course = new Course(req.body);
+//   const reply = new Reply({
+//     ...req.body,
+//   });
+//   try {
+//     reply.save();
+//     res.status(201).send(reply);
+//   } catch (error) {
+//     res.status(400).send(error);
+//   }
+// });
+// router.get("/replies", async (req, res) => {
+//   try {
+//     const replies = await Reply.find({}).sort({ createdAt: +1 });
+//     res.status(200).send(replies);
+//   } catch (error) {
+//     res.status(500).send(error);
+//   }
+// });
 
-router.delete("/replies/:id", async (req, res) => {
-  try {
-    // const course = await Course.findByIdAndDelete(req.params.id);
-    const comment = await Reply.findOneAndDelete({
-      _id: req.params.id,
-    });
-    if (!comment) {
-      return res.status(404).send();
-    }
-    res.send(comment);
-  } catch (error) {
-    res.status(500).send(error);
-  }
-});
-export default router;
+// router.delete("/replies/:id", async (req, res) => {
+//     try {
+//       // const course = await Course.findByIdAndDelete(req.params.id);
+//       const comment = await Reply.findOneAndDelete({
+//         _id: req.params.id,
+//       });
+//       if (!comment) {
+//         return res.status(404).send();
+//       }
+//       res.send(comment);
+//     } catch (error) {
+//       res.status(500).send(error);
+//     }
+//   });
+//   export default router;
