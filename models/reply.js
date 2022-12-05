868a1a351c5c554d3b4858d20100022d5c39fbfa
shhady <shhadyse@gmail.com>
2022-12-06 01:21:19 +0200

    addReply

---
+++ models/reply.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const ReplySchema = mongoose.Schema(
+  {
+    reply: { type: String },
+    replyOwnerId: { type: String },
+    playListId: { type: String },
+    userAvatar: { type: String },
+    courseOwnerId: { type: String },
+    videoId: { type: String },
+    firstName: { type: String },
+    lastName: { type: String },
+    commentId: { type: String },
+    read: { type: Boolean },
+    commentOwnerId: { type: String },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("Reply", ReplySchema);
