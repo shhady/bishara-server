35647f50a7ab582af07754db8e55e29a64749f87
shhady <shhadyse@gmail.com>
2022-12-06 13:32:38 +0200

    reply

--- routers/comment.js
+++ routers/comment.js
@@ -86,7 +86,7 @@ router.put("/comments/:id", async (req, res) => {
   res.status(200).send(replyUpdate);
 });
 
-router.delete("/comment/reply/:id", async (req, res) => {
+router.put("/comment/reply/:id", async (req, res) => {
   const replyId = req.body.replyId;
   // console.log(videoId);
   const commentToupdate = await Comment.findOneAndUpdate(
