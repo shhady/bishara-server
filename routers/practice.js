0fee9f6710feb684349ae7bc2d214f760af20a53
shhady <shhadyse@gmail.com>
2022-12-15 19:49:20 +0200

    replies

--- routers/practice.js
+++ routers/practice.js
@@ -63,30 +63,33 @@ router.get("/practices/:id", auth, async (req, res) => {
   }
 });
 
-router.put("/practice/:id", async (req, res) => {
+router.put("/practices/:id", async (req, res) => {
   const theVideoReply = req.body.theVideoReply;
   const videoName = req.body.videoName;
   const courseId = req.body.courseId;
   const nameOfProblem = req.body.nameOfProblem;
   const practiceId = req.body.practiceId;
-  console.log(reply);
-  const videoReply = await Practice.findOneAndUpdate(
-    { _id: req.params.id },
-    {
-      $push: {
-        videoReply: {
-          theVideoReply,
-          videoName,
-          courseId,
-          nameOfProblem,
-          practiceId,
+  try {
+    const videoReply = await Practice.findOneAndUpdate(
+      { _id: req.params.id },
+      {
+        $push: {
+          videoReply: {
+            theVideoReply,
+            videoName,
+            courseId,
+            nameOfProblem,
+            practiceId,
+          },
         },
-      },
-    }
-  );
-  await videoReply.save();
-  // console.log(firstName);
-  res.status(200).send(videoReply);
+      }
+    );
+    await videoReply.save();
+    // console.log(firstName);
+    res.status(200).send(videoReply);
+  } catch (error) {
+    res.status(404).send(error);
+  }
 });
 
 router.patch("/practices/:id", async (req, res) => {
