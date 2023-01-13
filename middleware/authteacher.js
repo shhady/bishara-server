f3ee39be96728a08f2bf56ac3ecf03b44d7aff2c
shhady <shhadyse@gmail.com>
2023-01-13 13:52:36 +0200

    auth

--- middleware/authteacher.js
+++ middleware/authteacher.js
@@ -1,47 +1,46 @@
+// import jwt from "jsonwebtoken";
+
+// const auth = async (req, res, next) => {
+//   try {
+//     const token = req.headers.authorization.split(" ")[1];
+//     const isCustomAuth = token.length < 500;
+
+//     let decodedData;
+//     if (token && isCustomAuth) {
+//       decodedData = jwt.verify(token, "test");
+
+//       req.userId = decodedData?.id;
+//     } else {
+//       decodedData = jwt.decode(token);
+//       req.userId = decodedData?.sub;
+//     }
+//     next();
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// export default auth;
 import jwt from "jsonwebtoken";
 import Teacher from "../models/teacher.js";
 
 const auth = async (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
+    const token = req.header("Authorization").replace("Bearer ", "");
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const teacher = await Teacher.findOne({
+      _id: decoded._id,
+      "tokens.token": token,
+    });
 
-    let decodedData;
-    if (token && isCustomAuth) {
-      decodedData = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-      req.userId = decodedData?.sub;
+    if (!teacher) {
+      throw new Error();
     }
+    req.token = token;
+    req.teacher = teacher;
     next();
   } catch (error) {
-    console.log(error);
+    res.status(401).send({ error: "please authenticate" });
   }
 };
-
 export default auth;
-// import jwt from "jsonwebtoken";
-// import Teacher from "../models/teacher.js";
-
-// const auth = async (req, res, next) => {
-//   try {
-//     const token = req.header("Authorization").replace("Bearer ", "");
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-//     const teacher = await Teacher.findOne({
-//       _id: decoded._id,
-//       "tokens.token": token,
-//     });
-
-//     if (!teacher) {
-//       throw new Error();
-//     }
-//     req.token = token;
-//     req.teacher = teacher;
-//     next();
-//   } catch (error) {
-//     res.status(401).send({ error: "please authenticate" });
-//   }
-// };
-// export default auth;
--- routers/teacher.js
+++ routers/teacher.js
@@ -210,16 +210,16 @@ router.patch("/teachers/me", auth, async (req, res) => {
   try {
     // const teacher = await Teacher.findById(req.params.id);
 
-    updates.forEach((update) => (req.teacher[update] = req.body[update]));
-    await req.teacher.save();
-    // const teacher = await Teacher.findByIdAndUpdate(req.params.id, req.body, {
-    //   new: true,
-    //   runValidators: true,
-    // });
+    // updates.forEach((update) => (req.teacher[update] = req.body[update]));
+    const teacher = await Teacher.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
 
-    // if (!teacher) {
-    //   res.status(404).send();
-    // }
+    if (!teacher) {
+      res.status(404).send();
+    }
+    await req.teacher.save();
     res.send(req.teacher);
   } catch (error) {
     res.status(400).send(error);
