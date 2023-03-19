df14e069d4d7a8b39cc69fcf1578e29d50659c4e
shhady <shhadyse@gmail.com>
2023-03-19 14:54:37 +0200

    mail

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const io = new Server(server, {
   cors: {
     origin: process.env.Origin_Cors,
     methods: ["GET", "POST","PUT", "PATCH","DELETE"],
-    allowedHeaders: ["Content-Type, Authorization"],
+    allowedHeaders: ["my-custom-header"],
     credentials:true
   },
 });
@@ -112,7 +112,11 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.Origin_Cors,
+  methods:'GET, POST, PUT, DELETE,PATCH, OPTIONS',
+  allowedHeaders:"Content-Type, Authorization"
+}));
 // app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(
