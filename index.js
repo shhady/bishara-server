5d1d4079b24966981b95abcc592599f6a846ef52
shhady <shhadyse@gmail.com>
2023-03-19 15:16:51 +0200

    mail

--- index.js
+++ index.js
@@ -112,11 +112,7 @@ io.on("connection", (socket) => {
   });
 });
 
-app.use(cors({
-  origin: process.env.Origin_Cors,
-  methods:'GET, POST, PUT, DELETE,PATCH, OPTIONS',
-  allowedHeaders:"Content-Type, Authorization"
-}));
+app.use(cors());
 // app.use(express.json());
 app.use(express.json({ limit: "10mb" }));
 app.use(
